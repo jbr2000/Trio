5273c81d4adfb9d5a63300d044da642c2e8d572e
jbr2000 <jbrupert@mail.csuchico.edu>
2019-11-15 05:13:32 -0800

    puts 3 numbers in order from greatest to least

--- main.cpp
+++ main.cpp
@@ -2,18 +2,21 @@
 using namespace std;
 
 // <-- ADD YOUR FUNCTION PROTOTYPE HERE
-
+void swap(int &x, int &y);
+void order(int &x, int &y, int &z);
 int main()
 {
   //DO NOT CHANGE WITHIN THIS AREA...
-  int red, blue, green;
+  int red;
+  int blue;
+  int green;
   cout<<"Enter Red, Green, and Blue values: ";
   cin>>red>>green>>blue;
   //...END OF "DO NOT CHANGE" AREA
 
 
   // <-- ADD YOUR FUNCTION CALL HERE
-
+  order(red,green,blue);
 
   //DO NOT CHANGE WITHIN THIS AREA...
   cout<<"Rearranged....\n";
@@ -23,3 +26,33 @@ int main()
 }
 
 // <-- ADD YOUR FUNCTION DEFINITON HERE
+void swap(int &x, int &y)
+{
+  int temp;
+
+  temp = x;
+  x = y;
+  y = temp;
+  return; 
+}  
+
+void order(int &x, int &y, int &z)
+{ 
+  if (z>x)
+  {
+    swap(x,z);
+  }  
+  if (y>x)
+  {
+    swap(y,x);
+  } 
+  if (z>y)
+  {        
+    swap(x,y);
+  }
+  return;
+}  
+
+
+
+
